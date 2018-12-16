9b3aebcc4c4f6c1d5e584b860aa505547bc7f014
zwiglm <martin.zwigl@gmx.at>
2018-12-16 22:13:29 +0100

    List of servers in config

--- Software/AlphaClient/AlphaClient/AlphaClient/Properties/Settings.Designer.cs
+++ Software/AlphaClient/AlphaClient/AlphaClient/Properties/Settings.Designer.cs
@@ -32,5 +32,16 @@ namespace AlphaClient.Properties {
                 return ((string)(this["FFMPEG_EXE_PATH"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
+            "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
+            "tring>192.168.8.103</string>\r\n</ArrayOfString>")]
+        public global::System.Collections.Specialized.StringCollection SERVERS {
+            get {
+                return ((global::System.Collections.Specialized.StringCollection)(this["SERVERS"]));
+            }
+        }
     }
 }
