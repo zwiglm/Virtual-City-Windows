e58e79c7322135510f8608419239447540fdbb80
zwiglm <martin.zwigl@gmx.at>
2018-12-16 20:07:54 +0100

    Initial commit from reference client

---
+++ Software/AlphaClient/AlphaClient/Services/AppInitializer.cs
@@ -0,0 +1,13 @@
+﻿using AlphaClient.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphaClient.Services
+{
+    public class AppInitializer : IAppInitializer
+    {
+    }
+}
